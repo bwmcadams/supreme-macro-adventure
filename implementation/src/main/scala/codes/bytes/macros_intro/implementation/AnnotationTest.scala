6a5e1449b5ace574dd0252895f455ee1453ca816
Brendan McAdams <brendan@boldradius.com>
2016-02-13 12:25:37 -0800

    Half way there...

--- implementation/src/main/scala/codes/bytes/macros_intro/implementation/AnnotationTest.scala
+++ implementation/src/main/scala/codes/bytes/macros_intro/implementation/AnnotationTest.scala
@@ -1,8 +1,14 @@
 package codes.bytes.macros_intro.implementation
 
-import codes.bytes.macros_intro.macros.hello
+import codes.bytes.macros_intro.macros.{ADT, hello}
 
 @hello
 object Test extends App {
   println(this.hello)
 }
+
+@ADT
+trait Bar
+
+@ADT
+object Foo
